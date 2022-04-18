777310cfc79119b61947c7ccc0dd46dd6627846b
davidcollins <david.collins001@gmail.com>
2022-04-18 21:28:08 +0100

    Set door event number in radio receiver since backup domain in stm32f1 isn't working

--- system/sbin/sensor_pub.py
+++ system/sbin/sensor_pub.py
@@ -22,10 +22,19 @@ def NO_sensor_1985242708(client, raw_data):
     # client.publish(sensor_queue, data['temperature'] / 100)
 
 
-def sensor_1985242708(client, raw_data):
-    sensor_queue = "state/sensor/1985242708"
-    sensor_keys = ['hwid', 'door_event']
-    # publish temp
-    data = dict(zip(sensor_keys, raw_data))
-    data['timestamp'] = dt.datetime.now().timestamp()
-    client.publish(sensor_queue, json.dumps(data))
+def setup_1985242708():
+    update_count = {"count": 0}
+
+    def sensor_1985242708(client, raw_data):
+        sensor_queue = "state/sensor/1985242708"
+        sensor_keys = ['hwid', 'door_event']
+        # publish temp
+        # data = dict(zip(sensor_keys, raw_data))
+        update_count["count"] += 1
+        data = dict(zip(sensor_keys, [raw_data[0], update_count["count"]]))
+        client.publish(sensor_queue, json.dumps(data))
+
+    return sensor_1985242708
+
+
+sensor_1985242708 = setup_1985242708()
