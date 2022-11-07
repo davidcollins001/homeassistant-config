13dc46031b6572b285a94733663d63e3a3fcb1a4
davidcollins <david.collins001@gmail.com>
2022-11-07 20:35:06 +0000

    Add notify and publish to state queue for hue  lights

--- system/sbin/bt_backend.py
+++ system/sbin/bt_backend.py
@@ -3,7 +3,6 @@ import time
 import pydbus
 import argparse
 import logging
-import datetime as dt
 import paho.mqtt.client as mqtt
 
 import setup_log  # noqa
@@ -14,23 +13,11 @@ import setup_log  # noqa
 #   the 2nd argument for get()
 
 
-# notifications:
-#
-#    import threading as th
-#    from gi.repository import GLib
-#
-#    loop=GLib.MainLoop()
-#    th.Thread(targe=loop.run)
-#    t=th.Thread(target=loop.run)
-#    char.onPropertiesChanged = callback
-#    char.StartNotify()
-#    t.start()
-
-
 logger = logging.getLogger("bt_backend")
 logger.setLevel(logging.WARN)
 
 DEFAULT_CMD_QUEUE = 'cmnd/lights/#'
+STATE_QUEUE = 'state/lights/{}/{}'
 
 # DBus object paths
 BLUEZ_SERVICE = 'org.bluez'
@@ -40,16 +27,17 @@ ADAPTER_PATH = '/org/bluez/hci0'
 def hue_lamp_update(device, command, message):
     connection = device.get("connection")[command]
 
-    def power(state):
-        connection.WriteValue([state], None)
+    valid_ranges = {
+        'POWER': (0, 1),
+        'Dimmer': (0, 255),
+    }
 
-    def dimmer(state):
-        msg = f"Invalid dimmer value: {state} must be > 0 and < 256"
-        assert (state > 0) and (state < 256), msg
-        connection.WriteValue([state], None)
-
-    cmds = {'POWER': power, 'Dimmer': dimmer}
-    cmds.get(command)(int(message.payload))
+    state = int(message.payload)
+    valid_range = valid_ranges.get(command)
+    msg = (f"Invalid {command.lower()} value: {state} must "
+           f"be between {valid_range[0]} and valid_range[1]")
+    assert (state >= valid_range[0]) and (state <= valid_range[1]), msg
+    connection.WriteValue([state], None)
 
 
 hue_lamp_1_update = hue_lamp_update
@@ -77,21 +65,30 @@ def led_strip_update(device, command, message):
         data = [0x7E, 5, 3, p, 6, 0xFF, 0xFF, 0, 0xEF]
     elif command == 'command':
         # pass data through to bt device
-        data = list(map(lambda n: int(n), message.payload))
+        data = list(map(int, message.payload))
 
     connection = device.get("connection")
     connection.WriteValue(bytes(data), None)
 
 
-def dbus_connect(peripheral_address, uuids):
-    def get_characteristic(bus, dev_path, uuid):
+def dbus_connect(peripheral_address, uuids, callback=None):
+    def get_characteristic(bus, dev_path, uuid_name, uuid):
         """Look up DBus path for characteristic UUID"""
         mngr = bus.get(BLUEZ_SERVICE, '/')
         mng_objs = mngr.GetManagedObjects()
         for path in mng_objs:
-            chr_uuid = mng_objs[path].get('org.bluez.GattCharacteristic1', {}).get('UUID')
+            chr_uuid = mng_objs[path].get('org.bluez.GattCharacteristic1',
+                                          {}).get('UUID')
             if path.startswith(dev_path) and chr_uuid == uuid.casefold():
-                return bus.get(BLUEZ_SERVICE, path)
+                char = bus.get(BLUEZ_SERVICE, path)
+                if callback:
+                    # import pdb; pdb.set_trace()  # noqa
+                    fn, mqtt, dev_name = callback
+                    char.onPropertiesChanged = fn(
+                        STATE_QUEUE.format(dev_name, uuid_name), mqtt
+                    )
+                    char.StartNotify()
+                return char
 
     bus = pydbus.SystemBus()
     # adapter = bus.get(BLUEZ_SERVICE, ADAPTER_PATH)
@@ -103,7 +100,7 @@ def dbus_connect(peripheral_address, uuids):
         raise Exception("Connection failed")
 
     if isinstance(uuids, dict):
-        chars = {uuid_name: get_characteristic(bus, device_path, uuid)
+        chars = {uuid_name: get_characteristic(bus, device_path, uuid_name, uuid)
                  for uuid_name, uuid in uuids.items()}
     else:
         chars = get_characteristic(bus, device_path, uuids)
@@ -112,7 +109,7 @@ def dbus_connect(peripheral_address, uuids):
 
 
 def ble_connect(fn):
-    def inner(devices, message, c):
+    def inner(devices, message, callback):
         *base_topic, dev_name, command = message.topic.split('/')
         # ignore mqtt messages for other lights
         if not devices.get(dev_name):
@@ -123,91 +120,70 @@ def ble_connect(fn):
                 if not devices[dev_name].get("connection"):
                     mac = devices[dev_name]['mac']
                     uuids = devices[dev_name]['uuids']
-                    devices[dev_name]["connection"] = dbus_connect(mac, uuids)
-
-                return fn(devices, message, c)
+                    devices[dev_name]["connection"] = dbus_connect(mac, uuids,
+                                                                   callback +
+                                                                   (dev_name,))
+                return fn(devices, message)
             except Exception as e:
                 logger.error(f"failed to write ({e}), reconnecting: {dev_name}")
-                time.sleep(0.5)
+                time.sleep(0.2)
                 # if conn:
                     # conn.disconnect()
                 if "connection" in devices[dev_name]:
                     del devices[dev_name]["connection"]
                 devices[dev_name]["connection"] = None
 
+        logger.warning("failed to switch")
+
     return inner
 
 
 @ble_connect
-def dispatch_message(devices, message, c):
+def dispatch_message(devices, message):
     logger.info(f"received payload on {message.topic}: {message.payload}")
     *base_topic, dev_name, command = message.topic.split('/')
+    # import pdb; pdb.set_trace()  # noqa
     device = devices.get(dev_name)
     update = globals()[f"{dev_name}_update"]
     return update(device, command, message)
+
+
+# def update_cb(m, c):
+def update_cb(topic, c):
+    print("setup callback: ", topic)
+
+    def inner(char, data, *args):
+        # org.bluez.GattCharacteristic1 {'Notifying': True} []
+        print(char, type(char))
+        print(topic, char, data, *args)
+        # TODO: what is characteristic to get notification value update
+        value = data.get("Value", None)
+        if value:
+            # c.publish(m.topic.replace('cmnd', 'state'), value[0])
+            c.publish(topic, value[0])
+    return inner
 
 
 def manager(devices, device="hci0", queue=DEFAULT_CMD_QUEUE):
     # on reconnect subscriptions will be renewed
     client = mqtt.Client()
     client.on_connect = lambda c, d, f, rc: c.subscribe(queue)
-    client.on_message = lambda c, d, m: dispatch_message(devices, m, c)
+    client.on_message = lambda c, d, m: dispatch_message(devices, m,
+                                                         (update_cb, c))
+    # import pdb; pdb.set_trace()  # noqa
     client.connect("homeassistant", 1883, 60)
     logger.info(f"connected to {queue}")
 
     # block and process network traffic, callbacks and reconnecting
-    client.loop_forever()
-    # client.loop_start()
+    # client.loop_forever()
+    client.loop_start()
     # client.loop_stop()
 
-    return client
-
-
-def notify():
-    import sys
-    import time
-    import pydbus
-    import argparse
-    import logging
-    import datetime as dt
-    import paho.mqtt.client as mqtt
-    import threading as th
     from gi.repository import GLib
-    BLUEZ_SERVICE = 'org.bluez'
-    ADAPTER_PATH = '/org/bluez/hci0'
+    mainloop = GLib.MainLoop()
+    mainloop.run()
 
-    def get_characteristic(bus, dev_path, uuid):
-        """Look up DBus path for characteristic UUID"""
-        mngr = bus.get(BLUEZ_SERVICE, '/')
-        mng_objs = mngr.GetManagedObjects()
-        for path in mng_objs:
-            chr_uuid = mng_objs[path].get('org.bluez.GattCharacteristic1', {}).get('UUID')
-            if path.startswith(dev_path) and chr_uuid == uuid.casefold():
-                return bus.get(BLUEZ_SERVICE, path)
-
-    def notify(*a, **k):
-      print(a, k)
-
-    loop=GLib.MainLoop()
-    t = th.Thread(target=loop.run)
-
-    bus = pydbus.SystemBus()
-    addr="FE:BF:DA:ED:C2:50"
-    device_path = f"{ADAPTER_PATH}/dev_{addr.replace(':', '_')}"
-    char_p = get_characteristic(bus, device_path, "932c32bd-0002-47a2-835a-a8d455b859dd")
-    char_d = get_characteristic(bus, device_path, "932c32bd-0003-47a2-835a-a8d455b859dd")
-    addr="21:04:77:64:04:AA"
-    device_path = f"{ADAPTER_PATH}/dev_{addr.replace(':', '_')}"
-    # char_l = get_characteristic(bus, device_path, "0000fff3-0000-1000-8000-00805f9b34fb")
-    char_l = get_characteristic(bus, device_path, "0000fff4-0000-1000-8000-00805f9b34fb")
-
-    char_p.onPropertiesChanged = notify
-    char_d.onPropertiesChanged = notify
-    char_l.onPropertiesChanged = notify
-    char_p.StartNotify()
-    char_d.StartNotify()
-    char_l.StartNotify()
-    t.start()
+    return client
 
 
 def parse_args(args=sys.argv):
@@ -237,6 +213,8 @@ if __name__ == "__main__":
 
     if args.manager:
         manager(devices, queue=DEFAULT_CMD_QUEUE)
-    elif args.command is not None:
-        p = args.power
-        _test_command()
+    # elif args.command is not None:
+        # p = args.power
+        # _test_command()
+    elif args.notify:
+        notify()
